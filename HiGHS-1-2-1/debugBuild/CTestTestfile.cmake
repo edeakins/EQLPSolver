6916b8de3dcf53590f941099d47f4dccbe6defa5
edeakins <edeakins@vols.utk.edu>
2022-04-07 11:23:33 -0500

    Time to transition to HiGHS v1.2.1 :(

---
+++ HiGHS-1-2-1/debugBuild/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/edeakins/LP/EQLPSolver/HiGHS-1-2-1
+# Build directory: /home/edeakins/LP/EQLPSolver/HiGHS-1-2-1/debugBuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("app")
+subdirs("check")
+subdirs("src")
--- NHiGHS/src/presolve/OCAggregate.cpp
+++
@@ -1,1 +0,0 @@
-#include "OCAggregate.h"
