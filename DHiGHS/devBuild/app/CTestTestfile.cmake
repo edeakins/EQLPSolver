427730d95cfc055a3ce967c5b30194ba1a47c06b
Ethan <edeakins@vols.utk.edu>
2022-08-01 13:08:42 -0400

    removed old HiGHs

--- DHiGHS/devBuild/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ethanjedidahdeakins/Work/LP/EQLPSolver/DHiGHS/app
-# Build directory: /Users/ethanjedidahdeakins/Work/LP/EQLPSolver/DHiGHS/devBuild/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- DHiGHS/devBuild/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ethanjedidahdeakins/Work/LP/EQLPSolver/DHiGHS/src
-# Build directory: /Users/ethanjedidahdeakins/Work/LP/EQLPSolver/DHiGHS/devBuild/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
