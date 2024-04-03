be4c08c9bbede4d3d812f3f8d5a58c44c46ca798
edeakins <edeakins33@gmail.com>
2024-04-03 18:52:23 -0400

    removing result jim folder and needed to modify run all shell file

--- TestScripts/run_benchmarks_all.sh
+++ TestScripts/run_benchmarks_all.sh
@@ -170,3 +170,12 @@ for f in ../MIPLIB-2017/*; do
    ../HiGHS-1-2-1/build/bin/highs "$f" --solver=ipm_alp_standard_crossover_iter --time_limit=3600 --report_time_file=run_method_17_MLIB;
 done
 
+# HS-COV-COD Instances
+for f in ../HS-COV-COD/*; do
+   ../HiGHS-1-2-1/build/bin/highs "$f" --solver=ipm_alp_primal_crossover_iter --time_limit=3600 --report_time_file=run_method_18_HCC;
+done
+
+# MIPLIB Instances
+for f in ../MIPLIB-2017/*; do
+   ../HiGHS-1-2-1/build/bin/highs "$f" --solver=ipm_alp_primal_crossover_iter --time_limit=3600 --report_time_file=run_method_18_MLIB;
+done
